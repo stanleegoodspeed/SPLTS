271392e4b869e9f291b63e8a2b89f574acf9ac9e
stanleegoodspeed <ccole3390@gmail.com>
2015-09-28 20:34:21 -0400

    config change

--- config/passport.js
+++ config/passport.js
@@ -12,22 +12,28 @@ var bcrypt          = require('bcrypt');
 //   database : 'TeamTrack'
 // });
 
-// var connection = mysql.createConnection({
-//   host     : 'virginia-mysql-instance1.ctlbtxqxbjwy.us-east-1.rds.amazonaws.com',
-//   port     : '3306',
-//   user     : 'colincole',
-//   password : 'SaltyTuna814',
-//   database : 'TeamTrack'
-// });
-
-var pool = mysql.createPool({
+var connection = mysql.createConnection({
   host     : 'virginia-mysql-instance1.ctlbtxqxbjwy.us-east-1.rds.amazonaws.com',
-  port     : 3306,
+  port     : '3306',
   user     : 'colincole',
   password : 'SaltyTuna814',
-  database : 'TeamTrack',
-  connectionLimit: 100,
-  supportBigNumbers: true
+  database : 'TeamTrack'
+});
+
+connection.connect(function(err) {
+    if(err)
+    {
+      console.log('HANNNNN: ' + err.code); // 'ECONNREFUSED'
+      console.log('SOLOOOO: ' + err.fatal); // true
+    }   
+});
+
+connection.query('SELECT 1', function(err) {
+    if(err)
+    {
+      console.log('HANNNNN: ' + err.code); // 'ECONNREFUSED'
+      console.log('SOLOOOO: ' + err.fatal); // true
+    }
 });
 
   
@@ -50,17 +56,9 @@ module.exports = function(passport) {
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-         pool.getConnection(function(err, connection) {
-    
-            if(err) { 
-              console.log(err); 
-              callback(true); 
-              return; 
-            }
 		connection.query('SELECT * FROM Users WHERE id = ?', id, function(err,rows){	
 			done(err, rows[0]);
 		});
-    });
     });
 	
 
@@ -83,14 +81,6 @@ module.exports = function(passport) {
         if(email.length == 0 || password.length == 0 || req.body.firstName.length == 0 || req.body.lastName.length == 0 || req.body.schoolID == 0 || req.body.referemail.length == 0) {
             return done(null, false, req.flash('signupMessage', 'All fields are required to complete sign up.'));
         }
-
-         pool.getConnection(function(err, connection) {
-    
-            if(err) { 
-              console.log(err); 
-              callback(true); 
-              return; 
-            }
 		// find a user whose email is the same as the forms email
 		// we are checking to see if the user trying to login already exists
         connection.query('SELECT * FROM Users WHERE email = ?', email, function(err,rows){
@@ -129,7 +119,6 @@ module.exports = function(passport) {
 				
             }	
 		});
-});
     }));
 
     // LOCAL Athlete SIGNUP
@@ -146,14 +135,6 @@ module.exports = function(passport) {
         if(email.length == 0 || password.length == 0 || req.body.firstName.length == 0 || req.body.lastName.length == 0 || req.body.schoolID == 0) {
             return done(null, false, req.flash('signupMessage', 'All fields are required to complete sign up.'));
         }
-
-        pool.getConnection(function(err, connection) {
-    
-            if(err) { 
-              console.log(err); 
-              callback(true); 
-              return; 
-            }
 
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
@@ -193,49 +174,7 @@ module.exports = function(passport) {
                 
             }   
         });
-        });
-}));
-
-
-        // // find a user whose email is the same as the forms email
-        // // we are checking to see if the user trying to login already exists
-        // connection.query('SELECT * FROM Users WHERE email = ?', email, function(err,rows){
-        //     if (err)
-        //         return done(err);
-        //      if (rows.length) {
-        //         return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-        //     } else {
-        //         // Check if the referral email exists
-        //         var runnerProfileCheck = connection.query('SELECT * FROM Runners WHERE fk_schoolID = ? AND firstName = ? AND lastName = ?', [req.body.schoolID, req.body.firstName, req.body.lastName], function(err,rows){
-
-        //             if (err)
-        //                 return done(err);
-        //             if (!rows.length) {
-        //                 return done(null, false, req.flash('signupMessage', 'There is no athlete profile to link to. In order for you to complete registration, your coach needs to use the Create New Athlete tool to build your running profile. Once completed, please check with your coach to make sure your name is spelled correctly, as this is used for the linking process.'));
-        //             } else {
-        //                 // if there is no user with that email
-        //                 // create the user
-        //                 var myUser          = new Object(); 
-        //                 myUser.email        = email;
-        //                 myUser.password     = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
-        //                 myUser.firstName    = req.body.firstName;
-        //                 myUser.lastName     = req.body.lastName;
-        //                 myUser.schoolID     = req.body.schoolID;
-        //                 myUser.runnerID     = rows[0].runnerID;
-        //                 myUser.userType     = 2;
-
-        //                 var query = connection.query('INSERT INTO Users (email, password, firstName, lastName, fk_schoolID, fk_runnerID, fk_userType) VALUES (?,?,?,?,?,?,?)', [myUser.email, myUser.password, myUser.firstName, myUser.lastName, myUser.schoolID, myUser.runnerID, myUser.userType], function(err, rows) {
-        //                   myUser.id = rows.insertId;
-        //                   return done(null, myUser);
-        //                 });
-        //             }
-
-        //         });
-
-                
-        //     }   
-        // });
-    //}));
+    }));
 
     // =========================================================================
     // LOCAL LOGIN =============================================================
@@ -251,64 +190,30 @@ module.exports = function(passport) {
     },
     function(req, email, password, done) { // callback with email and password from our form
 
-        pool.getConnection(function(err, connection) {
-    
-            if(err) { 
-              console.log(err); 
-              callback(true); 
-              return; 
+         connection.query('SELECT * FROM Users WHERE email = ?', email, function(err,rows){
+			if (err) {
+                console.log('the login error:' + err);
+                return done(null, false, req.flash('loginMessage', 'No user found.'));
             }
-
-            connection.query('SELECT * FROM Users WHERE email = ?', email, function(err,rows){
-                if (err) {
-                    console.log('the login error:' + err);
-                    return done(null, false, req.flash('loginMessage', 'No user found.'));
-                }
-                    
-                 if (!rows.length) {
-                    console.log('no user found');
-                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-                } 
                 
-                // if the user is found but the password is wrong
-                //!( rows[0].password == password)
-                //
-                if (!bcrypt.compareSync(password, rows[0].password)) {
-                    console.log('wrong password');
-                    return done(null, false, req.flash('loginMessage', 'Incorrect password.')); // create the loginMessage and save it to session as flashdata
-                }
-                    
-                
-                // all is well, return successful user
-                return done(null, rows[0]);         
-            
-            });
-        });
-
-  //        connection.query('SELECT * FROM Users WHERE email = ?', email, function(err,rows){
-		// 	if (err) {
-  //               console.log('the login error:' + err);
-  //               return done(null, false, req.flash('loginMessage', 'No user found.'));
-  //           }
-                
-		// 	 if (!rows.length) {
-  //               console.log('no user found');
-  //               return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-  //           } 
+			 if (!rows.length) {
+                console.log('no user found');
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+            } 
 			
-		// 	// if the user is found but the password is wrong
-  //           //!( rows[0].password == password)
-  //           //
-  //           if (!bcrypt.compareSync(password, rows[0].password)) {
-  //               console.log('wrong password');
-  //               return done(null, false, req.flash('loginMessage', 'Incorrect password.')); // create the loginMessage and save it to session as flashdata
-  //           }
+			// if the user is found but the password is wrong
+            //!( rows[0].password == password)
+            //
+            if (!bcrypt.compareSync(password, rows[0].password)) {
+                console.log('wrong password');
+                return done(null, false, req.flash('loginMessage', 'Incorrect password.')); // create the loginMessage and save it to session as flashdata
+            }
                 
 			
-  //           // all is well, return successful user
-  //           return done(null, rows[0]);			
+            // all is well, return successful user
+            return done(null, rows[0]);			
 		
-		// });
+		});
 		
 
 
